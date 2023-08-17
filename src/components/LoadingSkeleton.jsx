a3ddc90330544bef1a15f23ab0158de5e167d217
vladimir-loza <vlozaprado@gmail.com>
2023-08-17 02:32:43 -0600

    fix: vite config to netlify

--- src/components/LoadingSkeleton.jsx
+++ src/components/LoadingSkeleton.jsx
@@ -2,7 +2,7 @@ import {RestaurantSkeletonCard} from "./RestaurantSkeletonCard.jsx";
 
 export const LoadingSkeleton = () => {
   return (
-    <div className="container mx-auto flex flex-wrap gap-3">
+    <div className="container mx-auto flex flex-wrap gap-3 mt-16">
       {
         Array.from({length: 20}).map((_, idx) => (
           <RestaurantSkeletonCard key={idx}/>
